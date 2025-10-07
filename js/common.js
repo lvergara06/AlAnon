ec47534af98ecd4d5761362f6f124a200fff53bb
lvergara06 <lvergara06@users.noreply.github.com>
2025-10-07 23:43:19 +0000

    Deploy to GitHub pages

---
+++ js/common.js
@@ -0,0 +1,22 @@
+﻿function printInvoke() {
+    window.print();
+}
+
+function blazorGetTimezoneOffset() {
+    return new Date().getTimezoneOffset();
+}
+//window.blazorGetTimezoneOffset = function () {
+//    return new Date().getTimezoneOffset();
+//};
+
+window.onload = function () {
+    Fancybox.bind("[data-fancybox]", {
+        // Your custom options
+    });
+}
+
+function resizeCarouselItem(img) {
+    var itemContainer = img.parentElement.parentElement.parentElement.parentElement;
+    var itemHeight = img.height;
+    itemContainer.style.height = itemHeight + "px";
+}
